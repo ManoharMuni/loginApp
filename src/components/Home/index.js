40f2dc03b3d95b8a81daf48e55278bda65bdeacd
root <manoharmuni251@gmail.com>
2023-01-04 21:11:41 +0000

    loginApp

---
+++ src/components/Home/index.js
@@ -0,0 +1,36 @@
+// Write your code here
+import {Component} from 'react'
+
+import Message from '../Message'
+
+import Login from '../Login'
+
+import Logout from '../Logout'
+
+import './index.css'
+
+class Home extends Component {
+  state = {isLogin: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLogin: !prevState.isLogin}))
+  }
+
+  render() {
+    const {isLogin} = this.state
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <Message isLogin={isLogin} />
+          {isLogin ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
